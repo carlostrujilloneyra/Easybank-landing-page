6ba779e67b75471fec36d9de39abd7d23f0697c0
Carlos Trujillo <augusto21_20@hotmail.com>
2022-01-21 17:38:50 -0500

    Se agregó las clases de index.html al archivo animation.js para las animaciones

---
+++ JS/animation.js
@@ -0,0 +1,91 @@
+window.sr = ScrollReveal();
+
+// Animaciones para algunos elementos de "index.html"
+
+sr.reveal('.main-hero__info', {
+    duration: 1400,
+    origin: 'top',
+    distance: '400px'
+});
+
+sr.reveal('.main-hero__image', {
+    duration: 1400,
+    origin: 'left',
+    distance: '400px'
+});
+
+sr.reveal('.main__first-part', {
+    duration: 1500,
+    origin: 'left',
+    distance: '400px'
+});
+
+// Cajas de main
+
+sr.reveal('.main__box--first', {
+    duration: 1400,
+    origin: 'bottom',
+    distance: '400px'
+});
+
+sr.reveal('.main__box--second', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '600px'
+});
+
+sr.reveal('.main__box--third', {
+    duration: 1800,
+    origin: 'bottom',
+    distance: '800px'
+});
+
+
+sr.reveal('.main__box--four', {
+    duration: 1900,
+    origin: 'bottom',
+    distance: '1000px'
+});
+
+// Article
+
+sr.reveal('.main__subtitle--article', {
+    duration: 1400,
+    origin: 'bottom',
+    distance: '300px'
+});
+
+
+// Cards de Article
+
+sr.reveal('.main-article__card--first', {
+    duration: 1400,
+    origin: 'bottom',
+    distance: '400px'
+});
+
+sr.reveal('.main-article__card--second', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '600px'
+});
+
+sr.reveal('.main-article__card--third', {
+    duration: 1900,
+    origin: 'bottom',
+    distance: '800px'
+});
+
+sr.reveal('.main-article__card--four', {
+    duration: 2100,
+    origin: 'bottom',
+    distance: '1000px'
+});
+
+// Footer
+
+sr.reveal('.main-footer', {
+    duration: 1200,
+    origin: 'left',
+    distance: '800px'
+});
